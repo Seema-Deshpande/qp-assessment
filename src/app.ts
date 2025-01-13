9fe9a986b87363add541c5c468c5863a0c73a631
Seema Deshpande <seema.deshpande@encora.com>
2025-01-13 23:01:15 +0530

    Auth route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { json, urlencoded } from 'express';
 import { adminRoutes } from './routes/adminRoutes';
 import { userRoutes } from './routes/userRoutes';
+import { authRoutes } from './routes/authRoutes';
 import { config } from './config/config';
 import { connectToDatabase } from './database';
 
@@ -15,6 +16,7 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 
 // Routes
+app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/user', userRoutes);
 
